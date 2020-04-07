453537e632742b79aeb23953c423faae620ed51d
Michael Propp <michaelpropp8664@gmail.com>
2020-04-07 19:13:23 -0400

    Added numpy

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 This is a readme file
+
+You may need to install numpy. In the commandline, type "pip install numpy"
--- tsp-3510.py
+++ tsp-3510.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 input_file_name = sys.argv[1]  # inputfilename.txt
 output_file_name = sys.argv[2]  # outputfilename.txt
@@ -6,9 +7,14 @@ time = sys.argv[3]  # a rational number greater than 0
 
 # example: $ python3.7 tsp-3510.py mat-test.txt output.txt 5
 
+coordinates = np.loadtxt(input_file_name)  # loads data into coordinates
+print(coordinates)
+
+'''
 with open (input_file_name, 'r') as input_file:
     input_data = input_file.read()
     print(input_data)
+'''
 
 with open (output_file_name, 'w') as output_file:
     output_file.write("Output goes here")
